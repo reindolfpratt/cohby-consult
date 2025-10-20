80ee2eb3350fc5e0579a1c181903dd8a469465d8
reindolfpratt <reindolfpratt@gmail.com>
2025-10-20 15:43:10 +0100

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/cohby-consult/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/what-we-do" element={<WhatWeDo />} />
