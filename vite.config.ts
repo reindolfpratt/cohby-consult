2d09f7945cf9fb03e1d6b71947f8958975b5511a
reindolfpratt <reindolfpratt@gmail.com>
2025-10-20 20:08:23 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/cohby-consult/",  // Use "/" if using custom domain, or "/repo-name/" if using github.io
+  base: "/",  // Use "/" if using custom domain, or "/repo-name/" if using github.io
   plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
